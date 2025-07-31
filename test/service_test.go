3c39436a3f50959fce2513751a0f1cd52dd3ddf6
kidx45 <kidustessema91@gmail.com>
2025-07-31 13:20:29 +0300

    modified:   cmd/.env modified:   test/service_test.go

--- test/service_test.go
+++ test/service_test.go
@@ -10,38 +10,38 @@ import (
 )
 
 // Mock for outbound.Database
-type MockRepo struct {
+type MockDatabase struct {
 	mock.Mock
 }
 
-func (m *MockRepo) CreateTask(task domain.Task) (string, error) {
+func (m *MockDatabase) CreateTask(task domain.Task) (string, error) {
 	args := m.Called(task)
 	return args.String(0), args.Error(1)
 }
-func (m *MockRepo) GetByID(id string) (domain.Task, error) {
+func (m *MockDatabase) GetByID(id string) (domain.Task, error) {
 	args := m.Called(id)
 	return args.Get(0).(domain.Task), args.Error(1)
 }
-func (m *MockRepo) GetAll() ([]domain.Task, error) {
+func (m *MockDatabase) GetAll() ([]domain.Task, error) {
 	args := m.Called()
 	return args.Get(0).([]domain.Task), args.Error(1)
 }
-func (m *MockRepo) UpdateTask(task domain.Task) error {
+func (m *MockDatabase) UpdateTask(task domain.Task) error {
 	args := m.Called(task)
 	return args.Error(0)
 }
-func (m *MockRepo) Delete(id string) error {
+func (m *MockDatabase) Delete(id string) error {
 	args := m.Called(id)
 	return args.Error(0)
 }
 
 func TestServiceCreateTask(t *testing.T) {
-	mockRepo := new(MockRepo)
-	service := service.NewConnect(mockRepo)
-	task := domain.Task{Title: "Test", Description: "Desc"}
-	mockRepo.On("CreateTask", task).Return("123", nil)
+	mock := new(MockDatabase)
+	service := service.NewConnect(mock)
+	task := domain.Task{Title: "test1", Description: "testing123"}
+	mock.On("CreateTask", task).Return("1", nil)
 
 	id, err := service.CreateTask(task)
 	assert.NoError(t, err)
-	assert.Equal(t, "123", id)
+	assert.Equal(t, "1", id)
 }
